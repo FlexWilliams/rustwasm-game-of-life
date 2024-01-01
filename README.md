8b2af3d3c1ec9b806e88a31ecd2a8442e9682140
cube <cube@so-icy-ent.biz>
2024-01-01 14:04:07 -0700

    Getting GoL data directly from wasm memory and loading onto canvas

--- README.md
+++ README.md
@@ -42,3 +42,6 @@ Visit: [http://localhost:5173](http://localhost:5173)
 1. wasm-bindgen - allows JS/wasm to communicate with strings, JS objects, classes, etc, as opposed to purely integers and floats.
    1. Ex. https://rustwasm.github.io/wasm-bindgen/print.html#srclibrs-1
 2. wasm-pack vite-plugin: https://github.com/nshen/vite-plugin-wasm-pack
+3. Getting { memory } module to load since now using the `no bundler` strategy. See link below (kudos!):
+   1. https://lionturkey.github.io/posts/rustwasm/rustwasm.html
+   2. "In order to access memory used by Wasm without npm, we need to save the output of the init() function"
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,29 +1,101 @@
 <script lang="ts">
-	import init, { Universe } from 'game-of-life-wasm';
+	import init, { Cell, Universe } from 'game-of-life-wasm';
 	import { onMount } from 'svelte';
 
-	onMount(async () => {
-		init().then(() => {
-			const pre = document.getElementById('game-of-life-canvas');
+	let wasm: any; // TODO: add type of `InitOutput`
 
-			if (!pre) {
-				console.log('Pre element not found!');
-				return;
+	const CELL_SIZE = 5; // in px
+	const GRID_COLOR = '#CCCCCC';
+	const DEAD_COLOR = '#FFFFFF';
+	const ALIVE_COLOR = '#000000';
+
+	function drawGrid(context: CanvasRenderingContext2D, width: number, height: number): void {
+		context.beginPath();
+		context.strokeStyle = GRID_COLOR;
+
+		// Vertical lines.
+		for (let i = 0; i <= width; i++) {
+			context.moveTo(i * (CELL_SIZE + 1) + 1, 0);
+			context.lineTo(i * (CELL_SIZE + 1) + 1, (CELL_SIZE + 1) * height + 1);
+		}
+
+		// Horizontal lines.
+		for (let j = 0; j <= height; j++) {
+			context.moveTo(0, j * (CELL_SIZE + 1) + 1);
+			context.lineTo((CELL_SIZE + 1) * width + 1, j * (CELL_SIZE + 1) + 1);
+		}
+
+		context.stroke();
+	}
+
+	function getIndex(row: number, column: number, width: number): number {
+		return row * width + column;
+	}
+
+	function drawCells(
+		context: CanvasRenderingContext2D,
+		width: number,
+		height: number,
+		cellsPointer: number
+	) {
+		const cells = new Uint8Array(wasm.memory.buffer, cellsPointer, width * height);
+
+		context.beginPath();
+
+		for (let row = 0; row < height; row++) {
+			for (let col = 0; col < width; col++) {
+				const idx = getIndex(row, col, width);
+
+				context.fillStyle = cells[idx] === Cell.Dead ? DEAD_COLOR : ALIVE_COLOR;
+
+				context.fillRect(
+					col * (CELL_SIZE + 1) + 1,
+					row * (CELL_SIZE + 1) + 1,
+					CELL_SIZE,
+					CELL_SIZE
+				);
 			}
+		}
 
-			const universe = Universe.new();
+		context.stroke();
+	}
 
-			const renderLoop = () => {
-				pre.textContent = universe.render();
-				universe.tick();
+	onMount(async () => {
+		wasm = await init();
 
-				requestAnimationFrame(renderLoop);
-			};
+		const universe = Universe.new();
+		const width = universe.width();
+		const height = universe.height();
+		const cellsPointer = universe.cells();
 
-			// Tell the browser you wish to perform an animation (pretty plz)
-			// See: https://developer.mozilla.org/en-US/docs/Web/API/window/requestAnimationFrame
+		// Give the canvas room for all of our cells and a 1px border
+		// around each of them.
+		const canvas = document.getElementById('game-of-life-canvas') as HTMLCanvasElement;
+		if (!canvas) {
+			console.error('Canvas element not found!');
+			return;
+		}
+
+		canvas.height = (CELL_SIZE + 1) * height + 1;
+		canvas.width = (CELL_SIZE + 1) * width + 1;
+
+		const context = canvas.getContext('2d');
+		if (!context) {
+			console.error('Error getting 2D canvas ontext!');
+			return;
+		}
+
+		const renderLoop = () => {
+			universe.tick();
+
+			drawGrid(context, width, height);
+			drawCells(context, width, height, cellsPointer);
 			requestAnimationFrame(renderLoop);
-		});
+		};
+
+		// Tell the browser you wish to perform an animation (pretty plz)
+		// See: https://developer.mozilla.org/en-US/docs/Web/API/window/requestAnimationFrame
+		requestAnimationFrame(renderLoop);
 	});
 </script>
 
@@ -31,4 +103,4 @@
 	>This page contains webassembly and javascript content, please enable javascript in your browser.</noscript
 >
 <h1>Game of Life</h1>
-<pre id="game-of-life-canvas"></pre>
+<canvas id="game-of-life-canvas"></canvas>
